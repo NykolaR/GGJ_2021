cd4cb66100007d3f4fe840e77f6ba6899635c9d6
Adam Tilson <tilson3a@uregina.ca>
2021-01-30 12:17:28 -0600

    adam lobby

--- Scenes/Management/Boot.gd
+++ Scenes/Management/Boot.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _enter_tree() -> void:
-	var game : Node = load("res://Scenes/Management/Game.tscn").instance()
+	var game : Node = load("res://Scenes/Network/lobby.tscn").instance()
 
 	get_parent().call_deferred("add_child", game)
 	self.queue_free()
